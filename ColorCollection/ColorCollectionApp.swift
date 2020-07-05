c641702b40a14b22d1cf85759682382611bf7085
akatsuki174 <dev2015js@gmail.com>
2020-07-05 22:19:37 +0900

    add project

---
+++ ColorCollection/ColorCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCollectionApp.swift
+//  ColorCollection
+//
+//  Created by Akatsuki on 2020/07/05.
+//
+
+import SwiftUI
+
+@main
+struct ColorCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorCollection/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ColorCollection
+//
+//  Created by Akatsuki on 2020/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
