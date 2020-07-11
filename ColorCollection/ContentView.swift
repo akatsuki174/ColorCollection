0cb4bb60f5d69e5b5f209ef73a82398a0919d595
akatsuki174 <dev2015js@gmail.com>
2020-07-11 12:54:51 +0900

    make navigation tab

--- ColorCollection/ContentView.swift
+++ ColorCollection/ContentView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        TabView {
+            ExtractColorView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "a")
+                        Text("色の抽出")
+                    }
+                }.tag(1)
+            ColorListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "a")
+                        Text("色一覧")
+                    }
+                }.tag(2)
+            OthersView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "a")
+                        Text("その他")
+                    }
+                }.tag(3)
+        }
     }
 }
 
