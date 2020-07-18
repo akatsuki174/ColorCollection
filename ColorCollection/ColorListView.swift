73e5a9726d6b9d36ad004d124e7ba7ee6dab839a
akatsuki174 <dev2015js@gmail.com>
2020-07-18 23:30:33 +0900

    add dummy list

--- ColorCollection/ColorListView.swift
+++ ColorCollection/ColorListView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ColorListView: View {
+    var numberArray = ["1","2","3","4","5","6", "7"]
     var body: some View {
-        Text("ColorListView")
+        List(0..<numberArray.count) {i in
+            Text("\(numberArray[i])")
+        }
     }
 }
 
