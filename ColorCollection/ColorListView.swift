e110f4f0d3277ddaf5dc847c8b63327827d60a62
akatsuki174 <dev2015js@gmail.com>
2020-07-19 23:17:03 +0900

    basic layout

--- ColorCollection/ColorListView.swift
+++ ColorCollection/ColorListView.swift
@@ -11,7 +11,16 @@ struct ColorListView: View {
     var numberArray = ["1","2","3","4","5","6", "7"]
     var body: some View {
         List(0..<numberArray.count) {i in
-            Text("\(numberArray[i])")
+            HStack {
+                VStack {
+                    Text("#404040")
+                    Text("(64, 64, 64)")
+                }
+                VStack {
+                    Color(red: 64/255, green: 64/255, blue: 64/255)
+                }
+                .frame(width: 50, height: 50, alignment: .trailing)
+            }
         }
     }
 }
