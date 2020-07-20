1ddfafb1970f60673db557adfdf5b54d71de9465
akatsuki174 <dev2015js@gmail.com>
2020-07-20 23:32:27 +0900

    add button for color picker

--- ColorCollection/ExtractColorView.swift
+++ ColorCollection/ExtractColorView.swift
@@ -12,6 +12,7 @@ struct ExtractColorView: View {
     
     @State var image: UIImage?
     @State private var showingPhotoPicker = false
+    @State private var showingColorPicker = false
     
     var body: some View {
         VStack {
@@ -24,6 +25,14 @@ struct ExtractColorView: View {
                     .scaledToFit()
             }.sheet(isPresented: $showingPhotoPicker, onDismiss: nil) {
                 PhotoPicker(image: self.$image)
+            }
+            Button(action: {
+                self.showingColorPicker = true
+            }) {
+                Image(systemName: "plus")
+                    .frame(width: 24, height: 24, alignment: .center)
+            }.sheet(isPresented: $showingColorPicker, onDismiss: nil) {
+                // show color picker
             }
         }
         .padding([.leading, .trailing], 16)
