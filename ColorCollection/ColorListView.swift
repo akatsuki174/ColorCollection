83a59bb27204810038ce334c94914dbc023a7d42
akatsuki174 <dev2015js@gmail.com>
2020-08-22 21:26:23 +0900

    show color id and hex list

--- ColorCollection/ColorListView.swift
+++ ColorCollection/ColorListView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ColorListView: View {
     var numberArray = ["1","2","3","4","5","6", "7"]
+    
+    @FetchRequest(
+        entity: SavedColorMO.entity(),
+        sortDescriptors: []
+    ) var savedColors: FetchedResults<SavedColorMO>
+    
     var body: some View {
-        List(0..<numberArray.count) {i in
+        List(0 ..< savedColors.count) { i in
             HStack {
                 VStack {
-                    Text("#404040")
-                    Text("(64, 64, 64)")
+                    Text("\(savedColors[i].id)")
+                    Text("\(savedColors[i].hex)")
                 }
                 VStack {
                     Color(red: 64/255, green: 64/255, blue: 64/255)
